613056d7f3866fdf2681a0c9a61a6f005543cb73
Chelsea <chelseaeverest7@gmail.com>
2024-02-04 14:22:14 -0500

    Basic sass stuff

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div className="container container__home">
+      <main className="personal">
+        
+      </main>
+      <div className="content">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Home;
