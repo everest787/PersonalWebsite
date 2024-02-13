2abca20e98eb8fdf77a8d240f240b7270792aa97
Chelsea <chelseaeverest7@gmail.com>
2024-02-13 12:30:02 -0500

    Adding border to about me component

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,9 +1,11 @@
 function AboutMe() { 
     return (  
-        <div className="aboutme__content">
-            <h2 className="aboutme__heading">About Me</h2>
-            <div className="aboutme__desc-container">
-                <h3 className="aboutme__description">oh hi</h3>
+        <div className="aboutme">
+            <div className="aboutme__content">
+                <h2 className="aboutme__heading">About Me</h2>
+                <div className="aboutme__desc-container">
+                    <h3 className="aboutme__description">oh hi</h3>
+                </div>
             </div>
         </div>
     );
