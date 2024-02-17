0c726e5afa63de1bbf73f9ca36d9423458bf09bc
Chelsea <chelseaeverest7@gmail.com>
2024-02-17 14:59:35 -0500

    contact foundations

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,22 @@ function Contact() {
             <div className="contact__content">
                 <h2 className="contact__heading">Contact Me</h2>
                 <div className="contact__desc-container">
-                    <h3 className="contact__description">oh hi</h3>
+                    <div className="contact__email">
+                        <h2 className="contact__title">Email:</h2>
+                        <h1 className="contact__value">chelseaeverest8@gmail.com</h1>
+                    </div>
+                    <div className="contact__linkedin">
+                        <h2 className="contact__title">LinkedIn:</h2>
+                        <h1 className="contact__value">
+                            <a href="https://www.linkedin.com/in/chelsea-everest/">chelsea-everest</a>
+                        </h1>
+                    </div>
+                    <div className="contact__github">
+                        <h2 className="contact__title">Github:</h2>
+                        <h1 className="contact__value">
+                            <a href="https://github.com/everest787">everest787</a>
+                        </h1>
+                    </div>
                 </div>
             </div>
         </div>
