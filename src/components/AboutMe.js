8cb799105e1ee3258676bb8ea91fd9bb38afa50e
Chelsea <chelseaeverest7@gmail.com>
2024-02-19 13:01:59 -0500

    About me foundations

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,8 +3,52 @@ function AboutMe() {
         <div className="aboutme">
             <div className="aboutme__content">
                 <h2 className="aboutme__heading">About Me</h2>
-                <div className="aboutme__desc-container">
-                    <h3 className="aboutme__description">oh hi</h3>
+                <div className="aboutme__container">
+                    <div className="aboutme__section aboutme__section1">
+                        <h3 className="aboutme__section__heading">
+                            My favourite colour is green
+                        </h3>
+                    </div>
+                    <div className="aboutme__section aboutme__section2">
+                        <h3 className="aboutme__section__heading">
+                            I have a dog named Maya
+                        </h3>
+                    </div>
+                    <div className="aboutme__section aboutme__section3">
+                        <h3 className="aboutme__section__heading">
+                            Spring is my favourite season
+                        </h3>
+                    </div>
+                    <div className="aboutme__section aboutme__section4">
+                        <h3 className="aboutme__section__heading">
+                            I can lick my elbow
+                        </h3>
+                    </div>
+                    <div className="aboutme__section aboutme__section5">
+                        <h3 className="aboutme__section__heading">
+                            I have a Bachelor of Engineering Science in Software Engineering
+                        </h3>
+                    </div>
+                    <div className="aboutme__section aboutme__section6">
+                        <h3 className="aboutme__section__heading">
+                            Factorio is my favourite video game
+                        </h3>
+                    </div>
+                    <div className="aboutme__section aboutme__section7">
+                        <h3 className="aboutme__section__heading">
+                            Mint ice cream isn't very good
+                        </h3>
+                    </div>
+                    <div className="aboutme__section aboutme__section8">
+                        <h3 className="aboutme__section__heading">
+                            I enjoy listening to music (Alternative, Rock, Indie, and Pop mainly)
+                        </h3>
+                    </div>
+                    <div className="aboutme__section aboutme__section9">
+                        <h3 className="aboutme__section__heading">
+                            Ketchup on Kraft Dinner is good actually
+                        </h3>
+                    </div>
                 </div>
             </div>
         </div>
