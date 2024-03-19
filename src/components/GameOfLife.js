a617d379c7c03cbef5a0fa7b90547bcc064b9b73
Chelsea <chelseaeverest7@gmail.com>
2024-03-19 17:13:39 -0400

    Functioning

--- src/components/GameOfLife.js
+++ src/components/GameOfLife.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 function GameOfLife() { 
-    var rows = Math.floor((window.innerHeight - 40) / 20);
+    var rows = Math.floor((window.innerHeight - 20) / 10);
     var cols = Math.floor((window.innerWidth - 20) / 10);
 
     var playing = false;
@@ -84,6 +84,144 @@ function GameOfLife() {
         }
     }
 
+    
+    function updateView() {
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; j++) {
+                var cell = document.getElementById(i + "_" + j);
+                if (grid[i][j] === 0) {
+                    cell.setAttribute("class", "dead");
+                } else {
+                    cell.setAttribute("class", "live");
+                }
+            }
+        }
+    }
+
+    function randomButtonHandler() {
+        console.log(playing)
+        if (playing) return;
+        clearButtonHandler();
+        console.log(rows)
+        console.log(cols)
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; j++) {
+                var isLive = Math.round(Math.random());
+                if (isLive === 1) {
+                    var cell = document.getElementById(i + "_" + j);
+                    cell.setAttribute("class", "live");
+                    grid[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    // clear the grid
+    function clearButtonHandler() {
+        console.log("Clear the game: stop playing, clear the grid");
+        
+        playing = false;
+        clearTimeout(timer);
+        
+        var cellsList = document.getElementsByClassName("live");
+        // convert to array first, otherwise, you're working on a live node list
+        // and the update doesn't work!
+        var cells = [];
+        for (var i = 0; i < cellsList.length; i++) {
+            cells.push(cellsList[i]);
+        }
+        
+        for (var i = 0; i < cells.length; i++) {
+            cells[i].setAttribute("class", "dead");
+        }
+        resetGrids();
+    }
+
+    // start/pause/continue the game
+    function startButtonHandler() {
+        if (playing) {
+            playing = false;
+            clearTimeout(timer);
+        } else {
+            playing = true;
+            play();
+        }
+    }
+
+    // run the life game
+    function play() {
+        computeNextGen();
+        
+        if (playing) {
+            timer = setTimeout(play, reproductionTime);
+        }
+    }
+
+    function computeNextGen() {
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; j++) {
+                applyRules(i, j);
+            }
+        }
+        
+        // copy NextGrid to grid, and reset nextGrid
+        copyAndResetGrid();
+        // copy all 1 values to "live" in the table
+        updateView();
+    }
+
+    // RULES
+    // Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+    // Any live cell with two or three live neighbours lives on to the next generation.
+    // Any live cell with more than three live neighbours dies, as if by overcrowding.
+    // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+    function applyRules(row, col) {
+        var numNeighbors = countNeighbors(row, col);
+        if (grid[row][col] === 1) {
+            if (numNeighbors < 2) {
+                nextGrid[row][col] = 0;
+            } else if (numNeighbors === 2 || numNeighbors === 3) {
+                nextGrid[row][col] = 1;
+            } else if (numNeighbors > 3) {
+                nextGrid[row][col] = 0;
+            }
+        } else if (grid[row][col] === 0) {
+                if (numNeighbors === 3) {
+                    nextGrid[row][col] = 1;
+                }
+            }
+        }
+        
+    function countNeighbors(row, col) {
+        var count = 0;
+        if (row-1 >= 0) {
+            if (grid[row-1][col] === 1) count++;
+        }
+        if (row-1 >= 0 && col-1 >= 0) {
+            if (grid[row-1][col-1] === 1) count++;
+        }
+        if (row-1 >= 0 && col+1 < cols) {
+            if (grid[row-1][col+1] === 1) count++;
+        }
+        if (col-1 >= 0) {
+            if (grid[row][col-1] === 1) count++;
+        }
+        if (col+1 < cols) {
+            if (grid[row][col+1] === 1) count++;
+        }
+        if (row+1 < rows) {
+            if (grid[row+1][col] === 1) count++;
+        }
+        if (row+1 < rows && col-1 >= 0) {
+            if (grid[row+1][col-1] === 1) count++;
+        }
+        if (row+1 < rows && col+1 < cols) {
+            if (grid[row+1][col+1] === 1) count++;
+        }
+        return count;
+    }
+
 
     useEffect(() => {
         initialize();
@@ -95,9 +233,9 @@ function GameOfLife() {
             </div>
             
             <div className="controls">
-                <button id="start" /*onClick={() => startButtonHandler()}*/><span>Start</span></button>
-                <button id="clear" /*onClick={() => clearButtonHandler()}*/><span>Clear</span></button>
-                <button id="random" /*onClick={() => randomButtonHandler()}*/><span>Random</span></button>
+                <button id="start" onClick={() => startButtonHandler()}><span>Start</span></button>
+                <button id="clear" onClick={() => clearButtonHandler()}><span>Clear</span></button>
+                <button id="random" onClick={() => randomButtonHandler()}><span>Random</span></button>
             </div>
         </div>
     );
