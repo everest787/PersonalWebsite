2f204d1a768a3a6d2f00b164bf36891d557ac754
Chelsea <chelseaeverest7@gmail.com>
2024-03-27 14:50:57 -0400

    Updated work technologies section

--- src/components/WorkHistory.js
+++ src/components/WorkHistory.js
@@ -12,15 +12,16 @@ function WorkHistory() {
                 <div className="workhistory__job1">
                     <h2 className="workhistory__title">Junior Backend Developer</h2>
                     <ul className="workhistory__job-desc">
-                        <li className="workhistory__job-desc__item">Designed and developed scheduling 
-                        based software, including handling complex design issues with comprehensive 
-                        solutions.</li>
-                        <li className="workhistory__job-desc__item">Produced detailed user flow diagrams and project plans 
-                        for software features.</li>
-                        <li className="workhistory__job-desc__item">Prepared demonstrations of newly implemented features and 
-                        technologies.</li>
-                        <li className="workhistory__job-desc__item">Examined and maintained software quality through monitoring 
-                        of internal data analytics.</li>
+                        <li className="workhistory__job-desc__item">Contributed to a staffing platform enhancing 
+                        workforce management for shift-based and placement-based agencies, streamlining the 
+                        scheduling and job placement processes.</li>
+                        <li className="workhistory__job-desc__item">Designed and implemented a data upload feature 
+                        that reduced customer onboarding time by 35%.</li>
+                        <li className="workhistory__job-desc__item">Consistently upheld software quality standards 
+                        through monitoring, optimization of database queries, and refinement of software performance.</li>
+                        <li className="workhistory__job-desc__item">Led monthly team retrospective meetings 
+                        facilitating communication among team members, gathering feedback, and 
+                        collaborating with management to identify and implement solutions.</li>
                     </ul>
                 </div>
                 <div className="workhistory__job2">
@@ -28,22 +29,24 @@ function WorkHistory() {
                     <ul className="workhistory__job-desc">
                         <li className="workhistory__job-desc__item">Analyzed user / business needs to 
                         produce detailed functional and technical requirements.</li>
-                        <li className="workhistory__job-desc__item">Created software change documentation
-                         for features.
-                            <ul className="workhistory__job-desc">
-                                <li className="workhistory__job-desc__item">Reported results, elevated issues, and maintained formal 
-                            documentation.</li>
-                            </ul>
-                        </li>
+                        <li className="workhistory__job-desc__item">Developed software change documentation 
+                        for features, reporting outcomes, addressing concerns, and ensuring the upkeep 
+                        of formal documentation standards.</li>
+                        <li className="workhistory__job-desc__item">Conducted exhaustive software testing 
+                        for business applications, ensuring robustness, reliability, and adherence to 
+                        quality standards throughout the development lifecycle.</li>
                     </ul>
                 </div>
                 <div className="workhistory__job3">
                     <h2 className="workhistory__title">Quality Assurance Automation Analyst, Intern</h2>
                     <ul className="workhistory__job-desc">
-                        <li className="workhistory__job-desc__item">Utilized business and technical 
-                        requirements to develop behaviour driven test automation.</li>
-                        <li className="workhistory__job-desc__item">Demonstrated recently integrated 
-                        automation features to management and other teams.</li>
+                        <li className="workhistory__job-desc__item">Implemented behaviour driven test 
+                        automation that increased test coverage and reduced manual testing time.</li>
+                        <li className="workhistory__job-desc__item">Utilized both business and technical 
+                        requirements to develop, maintain, and update automated testing programs.</li>
+                        <li className="workhistory__job-desc__item">Managed the execution of automated 
+                        test scripts, providing demonstrations and explanations of the automation to 
+                        project leads and fellow QA analysts.</li>
                     </ul>
                 </div>
 
@@ -51,24 +54,94 @@ function WorkHistory() {
                     <h2 className="workhistory__technologies__text">Ingredients</h2>
                 </div>
                     
-                <div className="workhistory__job1-stack">
-                    <ul className="workhistory__stacklist">
-                        <li className="workhistory__stacklist__item">
-                        Javascript, NodeJs, TypeScript, React, Azure, AWS, Jira, MongoDB, GitHub, API, Agile
+                <div className="workhistory__job__stack">
+                    <h3 className="workhistory__job__stack__title-1">Programming</h3>
+                    <ul className="workhistory__job__stack__list">
+                        <li className="workhistory__job__stack__list__item">
+                        JavaScript
+                        </li>
+                        <li className="workhistory__job__stack__list__item">
+                        TypeScript
+                        </li>
+                    </ul>
+                    <ul className="workhistory__job__stack__list">
+                        <li className="workhistory__job__stack__list__item">
+                        Node.js
+                        </li>
+                        <li className="workhistory__job__stack__list__item">
+                        React.js
+                        </li>
+                        <li className="workhistory__job__stack__list__item">
+                        MongoDB
+                        </li>
+                    </ul>
+                    <h3 className="workhistory__job__stack__title-2">Technologies</h3>
+                    <ul className="workhistory__job__stack__list">
+                        <li className="workhistory__job__stack__list__item">
+                        Azure
+                        </li>
+                        <li className="workhistory__job__stack__list__item">
+                        AWS
+                        </li>
+                    </ul>
+                    <ul className="workhistory__job__stack__list">
+                        <li className="workhistory__job__stack__list__item">
+                        Jira
                         </li>
                     </ul>
                 </div>
-                <div className="workhistory__job2-stack">
-                    <ul className="workhistory__stacklist">
-                        <li className="workhistory__stacklist__item">
-                        Java, MySQL, Micro-services, Test Cases, Jira, GitHub
+                <div className="workhistory__job__stack">
+                    <h3 className="workhistory__job__stack__title-1">Technologies</h3>
+                    <ul className="workhistory__job__stack__list">
+                        <li className="workhistory__job__stack__list__item">
+                        Java
+                        </li>
+                        <li className="workhistory__job__stack__list__item">
+                        MySQL
+                        </li>
+                    </ul>
+                    <ul className="workhistory__job__stack__list">
+                        <li className="workhistory__job__stack__list__item">
+                        Jira
+                        </li>
+                        <li className="workhistory__job__stack__list__item">
+                        Excel
                         </li>
                     </ul>
                 </div>
-                <div className="workhistory__job3-stack">
-                    <ul className="workhistory__stacklist">
-                        <li className="workhistory__stacklist__item">
-                        Java, MySQL, Selenium, GitHub, Gherkin, Micro-services, Junit, Gradle, HTML5, Agile
+                <div className="workhistory__job__stack">
+                    <h3 className="workhistory__job__stack__title-1">Programming</h3>
+                    <ul className="workhistory__job__stack__list">
+                        <li className="workhistory__job__stack__list__item">
+                        Java
+                        </li>
+                        <li className="workhistory__job__stack__list__item">
+                        Selenium
+                        </li>
+                    </ul>
+                    <ul className="workhistory__job__stack__list">
+                        <li className="workhistory__job__stack__list__item">
+                        MySQL
+                        </li>
+                    </ul>
+                    <h3 className="workhistory__job__stack__title-2">Technologies</h3>
+                    <ul className="workhistory__job__stack__list">
+                        <li className="workhistory__job__stack__list__item">
+                        Junit
+                        </li>
+                        <li className="workhistory__job__stack__list__item">
+                        Gradle
+                        </li>
+                    </ul>
+                    <ul className="workhistory__job__stack__list">
+                        <li className="workhistory__job__stack__list__item">
+                        HTML5
+                        </li>
+                        <li className="workhistory__job__stack__list__item">
+                        XML
+                        </li>
+                        <li className="workhistory__job__stack__list__item">
+                        JSON
                         </li>
                     </ul>
                 </div>
