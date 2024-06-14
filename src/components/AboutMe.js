e4c00cc34dd0381bc15064dfeb1254007cd53154
Chelsea <chelseaeverest7@gmail.com>
2024-06-14 14:59:41 -0400

    Content to the backs of the cards

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -21,7 +21,27 @@ function AboutMe() {
                                 </figure>
                             </div>
                             <div className="aboutme__section__card__side aboutme__section__card__side--back">
-                                <p className="aboutme__section__card__side--back__body">back</p>
+                                <div className="aboutme__section__card__side--back__body">
+                                    <h3>Things I like:</h3>
+                                    <div>
+                                        <ul>
+                                            <li>
+                                            Reading
+                                            </li>
+                                            <li>
+                                                Thunderstorms
+                                            </li>
+                                        </ul>
+                                        <ul>
+                                            <li>
+                                            Ice Cream
+                                            </li>
+                                            <li>
+                                               Camping 
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -38,7 +58,27 @@ function AboutMe() {
                                 </figure>
                             </div>
                             <div className="aboutme__section__card__side aboutme__section__card__side--back">
-                                <p className="aboutme__section__card__side--back__body">back</p>
+                            <div className="aboutme__section__card__side--back__body">
+                                    <h3>Things I dislike:</h3>
+                                    <div>
+                                        <ul>
+                                            <li>
+                                            Tomatoes
+                                            </li>
+                                            <li>
+                                            Mosquitoes
+                                            </li>
+                                        </ul>
+                                        <ul>
+                                            <li>
+                                            Stilettos
+                                            </li>
+                                            <li>
+                                            Manifestos
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -65,7 +105,31 @@ function AboutMe() {
                                 </figure>
                             </div>
                             <div className="aboutme__section__card__side aboutme__section__card__side--back">
-                                <p className="aboutme__section__card__side--back__body">back</p>
+                            <div className="aboutme__section__card__side--back__body">
+                                    <h3>Books I am Reading:</h3>
+                                    <div>
+                                        <ul className="books">
+                                            <li>
+                                            The Three-Body Problem
+                                                <h3>by Liu Cixin</h3>
+                                            </li>
+                                            <li>
+                                            The Phoenix Project
+                                                <h3>by Gene Kim, George Spafford, and Kevin Behr</h3>
+                                            </li>
+                                        </ul>
+                                        <ul className="books">
+                                            <li>
+                                            Frames of Mind
+                                                <h3>by Howard Gardner</h3>
+                                            </li>
+                                            <li>
+                                            One Hundred Years of Solitude
+                                                <h3>by Gabriel García Márquez</h3>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -82,7 +146,27 @@ function AboutMe() {
                                 </figure>
                             </div>
                             <div className="aboutme__section__card__side aboutme__section__card__side--back">
-                                <p className="aboutme__section__card__side--back__body">back</p>
+                            <div className="aboutme__section__card__side--back__body">
+                                    <h3>Places I Would Like to Visit:</h3>
+                                    <div>
+                                        <ul>
+                                            <li>
+                                            North Macedonia
+                                            </li>
+                                            <li>
+                                            India
+                                            </li>
+                                        </ul>
+                                        <ul>
+                                            <li>
+                                            South Carolina
+                                            </li>
+                                            <li>
+                                            Alpha Centauri 
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
