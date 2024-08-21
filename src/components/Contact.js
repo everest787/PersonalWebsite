b83c758db3e49c239819b1db84b9d72039555244
Chelsea <chelseaeverest7@gmail.com>
2024-08-21 11:55:29 -0400

    Name Change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,12 +10,12 @@ function Contact() {
                 <div className="contact__desc-container">
                     <div className="contact__email">
                         <h2 className="contact__title">Email:</h2>
-                        <h1 className="contact__value">chelseaeverest8@gmail.com</h1>
+                        <h1 className="contact__value">taliaeverest7@gmail.com</h1>
                     </div>
                     <div className="contact__linkedin">
                         <h2 className="contact__title">LinkedIn:</h2>
                         <h1 className="contact__value">
-                            <a href="https://www.linkedin.com/in/chelsea-everest/" target="_blank" rel="noreferrer">chelsea-everest</a>
+                            <a href="https://www.linkedin.com/in/talia-everest/" target="_blank" rel="noreferrer">talia-everest</a>
                         </h1>
                     </div>
                     <div className="contact__github">
